4eb554bb2fa4bfb3f82b9894db900c51d2465f4c
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-02-13 15:46:47 +0100

    Initial Commit

---
+++ myBudget/myBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBudgetApp.swift
+//  myBudget
+//
+//  Created by Kiko on 2023-02-13.
+//
+
+import SwiftUI
+
+@main
+struct myBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
