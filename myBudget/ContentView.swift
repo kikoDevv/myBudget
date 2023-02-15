3a3b0286a63cbd4cfc370fe7804ffa3d5e629dae
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-02-15 23:05:34 +0100

    sheet now working

--- myBudget/ContentView.swift
+++ myBudget/ContentView.swift
@@ -8,82 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var listan = [String:String]()
-    @State var listView:Bool = false
-    @State var inkomst = ""
-    
+    @State var dec = [String:String]()
+    @State var showSheet:Bool = false
+    @State var key = String()
+    @State var value = String()
     
     var body: some View {
-        ZStack{
-            if listView==false{
-                VStack{
-                    Text("hallow börja med din inkomst")
-                    TextField("skriv din inkomst", text: $inkomst)
-                        .padding()
-                        .background(Color.gray.opacity(0.3))
-                        .cornerRadius(20)
-                        .padding()
-                    Button("Nästa"){
-                        listView.toggle()
-                    }
-                    .background(Color.gray.opacity(0.2))
-                }
+        VStack{
+            Text(key)
+            ForEach(dec.sorted(by: >), id: \.key){ key, value in
+                Text(key)
+                Text(value)
             }
-            //==================list view=================
-            if listView{
-                NavigationView{
-                    List{
-                        //==========Section 1==============
-                        Section (header: Text("inkomst")){
-                            VStack{
-                                HStack{
-                                    Text("inkomst")
-                                    Spacer()
-                                    Text("sparande")
-                                }
-                                HStack{
-                                    Text("\(inkomst) Kr")
-                                    Spacer()
-                                    Text("sifror")
-                                }
-                            }
-                        }
-                        //===================section2=================
-                        Section (header: Text("gatakory")){
-                            ForEach(listan.sorted(by: >), id: \.key ){ key, value in
-                                Text("\(key)                                \(value)kr")
-                            }
-                            .onDelete(perform: delete)
-                        }
-                    }
-                    .navigationBarItems(leading: EditButton())
-                }
+            Button("add"){
+                dec[key] = "sfds"
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .cornerRadius(20)
+            .offset(y:200)
+            
+            
+            Button("sheet"){
+                showSheet.toggle()
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .cornerRadius(20)
+            .sheet(isPresented: $showSheet) {
+                secondView(kkey: $key)
             }
         }
-  
     }
-    //================funktioner===============================
-    func delete(at offsets: IndexSet){
-        if let ndx = offsets.first {
-            let item = listan.sorted(by: >)[ndx]
-            listan.removeValue(forKey: item.key)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    struct secondView:View{
+        @Binding var kkey:String
+        var body: some View{
+            TextField("key", text: $kkey)
         }
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
