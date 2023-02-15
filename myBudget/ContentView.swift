08643f9a9001daa7cf007b1c3484aed7de384fa3
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-02-15 21:13:43 +0100

    singel view

--- myBudget/ContentView.swift
+++ myBudget/ContentView.swift
@@ -9,30 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     @State var listan = [String:String]()
-    @State var showSecondView:Bool = false
-    @State var titel:String=""
-    @State var siffran:String=""
-    @State var btPressed:Bool = false
-    
+    @State var listView:Bool = false
+    @State var inkomst = ""
     
     
     var body: some View {
-        ZStack {
-            NavigationView{
-                List{
-                    ForEach(listan.sorted(by: >), id: \.key ){ key, value in
-                        Text("\(key)                                \(value)kr")
+        ZStack{
+            if listView==false{
+                VStack{
+                    Text("hallow börja med din inkomst")
+                    TextField("skriv din inkomst", text: $inkomst)
+                        .padding()
+                        .background(Color.gray.opacity(0.3))
+                        .cornerRadius(20)
+                        .padding()
+                    Button("Nästa"){
+                        listView.toggle()
                     }
-                    .onDelete(perform: delete)
+                    .background(Color.gray.opacity(0.2))
                 }
-                .sheet(isPresented: $showSecondView, content: {
-                    secondVeiw(titel: $titel, Siffra: $siffran, btPressed: $btPressed)
-                })
-                .navigationBarItems(trailing: Button("add"){
-                    showSecondView.toggle()
-                })
+            }
+            //==================list view=================
+            if listView{
+                NavigationView{
+                    List{
+                        //==========Section 1==============
+                        Section (header: Text("inkomst")){
+                            VStack{
+                                HStack{
+                                    Text("inkomst")
+                                    Spacer()
+                                    Text("sparande")
+                                }
+                                HStack{
+                                    Text("\(inkomst) Kr")
+                                    Spacer()
+                                    Text("sifror")
+                                }
+                            }
+                        }
+                        //===================section2=================
+                        Section (header: Text("gatakory")){
+                            ForEach(listan.sorted(by: >), id: \.key ){ key, value in
+                                Text("\(key)                                \(value)kr")
+                            }
+                            .onDelete(perform: delete)
+                        }
+                    }
+                    .navigationBarItems(leading: EditButton())
+                }
             }
         }
+  
     }
     //================funktioner===============================
     func delete(at offsets: IndexSet){
@@ -56,7 +84,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-       // secondVeiw(titel: .constant("xx"), Siffra: .constant("0"))
         ContentView()
     }
 }
